dd8a9d172ac713286a9463b1d4807bc4bd426940
James <j606888@gmail.com>
2022-07-30 15:09:57 +0800

    Signup Form - basic setup

---
+++ src/frontendmentor/SignupForm/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+`
+
+const index = () => {
+  return (
+    <Container>
+      index
+    </Container>
+  )
+}
+
+export default index
