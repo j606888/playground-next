7a13b2ff72f13911126a900e6a7b4fd456828696
James <j606888@gmail.com>
2022-07-30 15:21:23 +0800

    Signup Form - setup font-family and resetStyle

--- pages/_document.js
+++ pages/_document.js
@@ -14,6 +14,7 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Big+Shoulders+Display:wght@700&family=Inter:wght@400;700&family=Lexend+Decafamily=Big+Shoulders+Display:wght@700&family=Kumbh+Sans:wght@400;700&display=swap"
           rel="stylesheet"
         />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet" />
       </Head>
       <body>
         <Main />
--- src/frontendmentor/SignupForm/index.js
+++ src/frontendmentor/SignupForm/index.js
@@ -1,13 +1,31 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { ThemeProvider, createGlobalStyle } from 'styled-components'
 
-const Container = styled.div`
+const ResetStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: "Poppins", sans-serif;
+  }
 `
+const Container = styled.div`
+
+`
+
+const theme = {
+  red: "hsl(0, 100%, 74%)",
+  green: "hsl(154, 59%, 51%)",
+  blue: "hsl(248, 32%, 49%)",
+  darkBlue: "hsl(249, 10%, 26%) ",
+  grayishBlue: "hsl(246, 25%, 77%)",
+}
 
 const index = () => {
   return (
     <Container>
-      index
+      <ResetStyle />
+      <h1>Title</h1>
     </Container>
   )
 }
