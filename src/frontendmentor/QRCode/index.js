01eece8f0ce70cf59b9581b8f96738679e7270a9
James <j606888@gmail.com>
2022-07-31 23:18:20 +0800

    QR-Code basic setup

---
+++ src/frontendmentor/QRCode/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: hsl(212, 45%, 89%);
+  width: 100%;
+  min-height: 100vh;
+`
+
+const QRCode = () => {
+  return <Container>QRCode</Container>
+}
+
+export default QRCode
---
+++ stories/frontendmentor/QRCode.stories.js
@@ -0,0 +1,8 @@
+import QRCode from "../../src/frontendmentor/QRCode"
+
+export default {
+  title: "frontendmentor/QRCode",
+}
+
+export const Default = (args) => <QRCode {...args} />
+Default.args = {}
