948a45d647c6c59209b4831c888e7f46053a3a66
James <j606888@gmail.com>
2022-08-01 22:30:48 +0800

    Add font

--- pages/_document.js
+++ pages/_document.js
@@ -15,7 +15,7 @@ export default function Document() {
           rel="stylesheet"
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;400;500;600;700&display=swap"
           rel="stylesheet"
         />
         <link
