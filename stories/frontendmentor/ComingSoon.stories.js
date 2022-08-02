e81cefbe48f778595d8e2240efcf0fab1ea390a0
James <j606888@gmail.com>
2022-08-02 22:49:04 +0800

    ComingSoon - init

---
+++ stories/frontendmentor/ComingSoon.stories.js
@@ -0,0 +1,8 @@
+import ComingSoon from "../../src/frontendmentor/ComingSoon"
+
+export default {
+  title: "frontendmentor/ComingSoon",
+}
+
+export const Default = (args) => <ComingSoon {...args} />
+Default.args = {}
